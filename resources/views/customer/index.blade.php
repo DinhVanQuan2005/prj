09fe61d00505ea69c09bfcab874c6df297a0d287
ngocan29 <168067406+ngocan29@users.noreply.github.com>
2024-12-24 01:54:09 +0700

    Update index.blade.php

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mt-4">
-    <h1>Chào mừng, {{ $user->name }}</h1>
+    <h1>Chào mừng, {{ $user->username }}</h1>
     <p>Đây là trang chủ dành cho khách hàng.</p>
 </div>
 @endsection
