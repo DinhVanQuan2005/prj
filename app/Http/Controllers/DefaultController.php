ccdf10f2652e9013cbefe0fb8c671fc56cb94f74
ngocan29 <168067406+ngocan29@users.noreply.github.com>
2024-12-23 20:48:06 +0700

    Update DefaultController.php

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 
-class DefaultController extends Controller
-{
-    public function index()
-    {
+class DefaultController extends Controller {
+    public function index() {
        return view('layouts.app');
     }
 
-    public function account()
-    {
+    public function account() {
         $user = Auth::user();
         return view('default.account', compact('user'));
     }
