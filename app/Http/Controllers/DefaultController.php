0244bbaf272c65b61b3632e4dba423b82ffb20ad
ngocan29 <168067406+ngocan29@users.noreply.github.com>
2024-12-23 20:47:28 +0700

    Update DefaultController.php

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -7,19 +7,12 @@ class DefaultController extends Controller
 {
     public function index()
     {
-       return view('default.index');
+       return view('layouts.app');
     }
 
-    /**
-     * Trang thông tin tài khoản (chỉ dành cho người dùng đã đăng nhập).
-     * @return \Illuminate\View\View
-     */
     public function account()
     {
-        // Lấy thông tin người dùng hiện tại
         $user = Auth::user();
-
-        // Trả về giao diện thông tin tài khoản
         return view('default.account', compact('user'));
     }
 }
