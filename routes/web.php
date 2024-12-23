939521dc57d7c18c0cbd4f0ba065e59f6515e5fa
ngocan29 <168067406+ngocan29@users.noreply.github.com>
2024-12-24 05:46:28 +0700

    Update web.php (admin)

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('cart.add');
 // Nhóm route dành cho customer
 Route::prefix('customer')->middleware(['auth', 'role:customer'])->group(function () {
-    Route::get('/', [CustomerController::class, 'index'])->name('customer.home');
+    Route::get('/customer', [CustomerController::class, 'index'])->name('customer.home');
     //Thông tin tài khoản
     Route::get('/account', [CustomerController::class, 'account'])->name('customer.account');
     Route::get('/account/edit', [CustomerController::class, 'editAccount'])->name('customer.account.edit');
@@ -55,4 +55,12 @@ Route::prefix('customer')->middleware(['auth', 'role:customer'])->group(function
     // Chi tiết đơn hàng
     Route::get('/orders/{order}', [CustomerController::class, 'orderDetail'])->name('customer.orders.detail');
 });
-
+//Nhóm route dành cho admin
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/account', [AdminController::class, 'account'])->name('admin.account');
+    Route::get('/admin/users', [AdminController::class, 'manageUsers'])->name('admin.users');
+    Route::get('/admin/products', [AdminController::class, 'manageProducts'])->name('admin.products');
+    Route::get('/admin/orders', [AdminController::class, 'manageOrders'])->name('admin.orders');
+    // Route::post('/logout', [AdminController::class, 'logout'])->name('admin.logout'); 
+});
