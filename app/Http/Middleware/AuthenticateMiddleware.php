09b2ff05a4be1a7943629eb5fdb40598818d2220
ngocan29 <168067406+ngocan29@users.noreply.github.com>
2024-12-23 21:03:58 +0700

    Update AuthenticateMiddleware.php

--- app/Http/Middleware/AuthenticateMiddleware.php
+++ app/Http/Middleware/AuthenticateMiddleware.php
@@ -7,13 +7,8 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-class AuthenticateMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+class AuthenticateMiddleware {
+    
     public function handle(Request $request, Closure $next): Response
     {
       if(Auth::id()==null){
