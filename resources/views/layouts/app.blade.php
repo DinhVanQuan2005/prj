03e08e3719cd6829618026bbe20c75c63e7aa973
ngocan29 <168067406+ngocan29@users.noreply.github.com>
2024-12-23 07:32:21 +0700

    Update app.blade.php (Done)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,60 +26,44 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-                            </ul>
-                        </li>
                     </ul>
-                    <form class="d-flex me-2" >
-                        <button class="btn btn-outline-dark" type="button" >
-                            <i class="bi-cart-fill me-1"></i>
-                            Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                        </button>
+                    <-- Thanh tìm kiếm sản phẩm -->
+                    <form class="d-flex me-2">
+                        <input class="form-control me-2" type="search" placeholder="Tìm kiếm sản phẩm" aria-label="Search">
+                        <button class="btn btn-outline-dark" type="submit">Tìm</button>
                     </form>
-
-                    <div class="dropdown">
-                        <button class="btn btn-outline-dark dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-people-fill"></i>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                            <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
-                            <li><a class="dropdown-item" href="#!">Register</a></li>
-                            <li><hr class="dropdown-divider" /></li>
-                            <li><a class="dropdown-item" href="{{route('logout')}}">Logout</a></li>
-                        </ul>
-                    </div>
-
+                    <button class="btn btn-outline-dark mx-2">
+                        <i class="bi bi-person"></i>
+                        Đăng Nhập/Đăng Ký
+                    </button>
                 </div>
             </div>
         </nav>
 
-        <!-- Header-->
-        <header class="bg-dark py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop GenZ</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Thời trang & Mỹ phẩm</p>
-                </div>
+        <header class="bg-dark py-5 text-center text-white">
+            <div class="container px-4 px-lg-5">
+                <h1 class="display-4 fw-bolder">Grow & Up</h1>
+                <p class="lead fw-normal text-white-50 mb-0">Mang đến vẻ đẹp toàn diện</p>
             </div>
         </header>
 
-        <!-- Section-->
-        @yield('content')
-        <!-- Footer-->
-
+        <!-- nội dung trang -->
+        <main>
+            <div id="bodyContainer">
+                <?php echo $content; ?>
+            </div>
+        </main>
+                        
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2024</p></div>
+            <div class="container text-center text-white">
+                <p>Cảm ơn vì chọn cửa hàng của chúng mình là nơi đặt chân và tìm hiểu để nâng tầm vẻ đẹp của bạn!</p>
+                <p>&copy; 2024 Grow & Up</p>
+            </div>
         </footer>
-        <!-- Bootstrap core JS-->
+                        
+        <!-- Bootstrap <JS> -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="backend/js/scripts.js"></script>
+        <!-- JS chính -->
+        <script src="public/backend/js/scripts.js"></script>
     </body>
 </html>
